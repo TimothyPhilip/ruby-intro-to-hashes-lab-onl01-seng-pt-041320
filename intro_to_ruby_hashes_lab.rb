16cc25473e576d56baea6ffa12d51a26729d49cf
Philippe Timothe <philippetimothe@gmail.com>
2020-04-22 14:27:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,8 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {:railroads => {
-	    :pieces => 4
-	    :name => {}
+	    :pieces => 4,
+	    :name => {},
 	    :rent_in_dollars => {}
 	  }
   }
